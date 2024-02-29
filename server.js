69ae0e8227dcf03c37d5e68483582d2381ff5238
SAFWAN ALI <126313311+Safwan2003@users.noreply.github.com>
2024-02-29 14:47:49 +0500

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ const swaggerJsDocs = YAML.load(path.join(__dirname, 'config', 'api.yaml'));
 
 const app = express()
 
-app.use(express.json())
+app.use(express.json({limit: '50mb',extended: true}))
+app.use(express.urlencoded({ extended: true ,limit:'50mb'}));
+
 app.use(cors())
 
 app.get('/',(req,res)=>{
